2892cc76a5b5ccc3d3280e1c5bbbf42217ad03ae
agogeio <steven.aiello@agoge.io>
2022-10-30 21:45:18 -0400

    Beggning TCP Proxy

---
+++ Chapter2-BasicNetworkingTools/bhp_tcp_proxy.py
@@ -0,0 +1,27 @@
+# FOUND ON PAGE 19 of BHP
+
+import sys
+import socket 
+import threading
+
+HEX_FILTER = ''.join([(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
+
+def hex_dump(src, length=16, show=True):
+    if isinstance(src, bytes):
+        src = src.decode()
+
+
+    results = list()
+    for i in range(0, len(src), length):
+        word = str(src[i:i+length])
+
+        printable = word.translate(HEX_FILTER)
+        hexa = ' '.join([f'{ord(c):02X}' for c in word])
+        hexwidth = length*3
+        result.append(f'{i:04x} {hexa:<{hexwidth}} {printable}')
+    
+    if show:
+        for line in results:
+            print(line)
+        else:
+            return results
