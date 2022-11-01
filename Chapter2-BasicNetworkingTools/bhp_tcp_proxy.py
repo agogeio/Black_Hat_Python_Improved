5c9185553a1e5b87dd3fb8c2721f2e5115e70fc0
agogeio <steven.aiello@agoge.io>
2022-10-31 20:46:36 -0400

    Initial bug filled BHP code entered

--- Chapter2-BasicNetworkingTools/bhp_tcp_proxy.py
+++ Chapter2-BasicNetworkingTools/bhp_tcp_proxy.py
@@ -1,5 +1,6 @@
 # FOUND ON PAGE 19 of BHP
 
+from ast import arg
 import sys
 import socket
 import threading
@@ -53,4 +54,88 @@ def response_handler(buffer):
     return buffer
 
 
-hex_dump("This is informtion that we will convert to HEX A")
+def proxy_handler(client_socket, remote_host, remote_port, recieve_first):
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    if recieve_first:
+        remote_buffer = recieve_from(remote_socket)
+        hex_dump(remote_buffer)
+
+    remote_buffer = response_handler(remote_buffer)
+    if len(remote_buffer):
+        #? Why is this not an f string?
+        print("[<==] Sending %d bytes to localhost." % len(remote_buffer))
+        client_socket.send(remote_buffer)
+
+    while True:
+        local_buffer = recieve_from(client_socket)
+        if len(local_buffer):
+            #? Why is this not an f string?
+            line = "[==>] Recieved %d bytes to localhost." % len(local_buffer)
+            print(line)
+            hex_dump(local_buffer)
+
+        local_buffer = request_handler(local_buffer)
+        remote_socket.send(local_buffer)
+        print("[==>] Sent to remote")
+
+        remote_buffer = recieve_from(remote_socket)
+        if len(remote_buffer):
+            print("[<==] Recieved %d bytes to remote." % len(remote_buffer))
+            hex_dump(remote_buffer)
+
+            remote_buffer = response_handler(remote_buffer)
+            client_socket.send(remote_buffer)
+            print("[<==] Sent to localhost")
+
+        if not len(local_buffer) or not len(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            print("[*] No more data, closing connections")
+            break
+
+            
+def server_loop(local_host, local_port, remote_host, remote_port, recieve_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind((local_host, local_port))
+    except Exception as e:
+        print(f"Problem on bing {str(e)}")
+        print(f"[!!] Failed to listen on {local_host} on port {local_port}")
+        print(f"[!!] Check for other listening sickets or correct permissions")
+        sys.exit(0)
+
+    print(f"Listening on {local_host} on port {local_port}")
+    server.listen(10)
+    while True:
+        client_socket, addr = server.accept()
+        print(f"Recieved incoming connection from {local_host} on port {local_port}")
+
+        proxy_thread = threading.Thread(target=proxy_handler, args=(client_socket, remote_host, remote_port, recieve_first))
+        proxy_thread.start()
+
+
+def main():
+    if len(sys.argv[1:]) != 5:
+        print("Usage: ./bhp_tcp_proxy.py localhost localport", end='')
+        print("remotehost remoteport recieve_first")
+        print("Example: ./bhp_tcp_proxy.py 127.0.0.1 9000 10.12.132.1 9000 True")
+        sys.exit(0)
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+    remote_host = sys.argv[3]
+    remote_port = int(sys.argv[4])
+    recieve_first = sys.argv[5]
+
+    if "True" in recieve_first:
+        recieve_first = True
+    else:
+        recieve_first = False
+
+    server_loop(local_host, local_port, remote_host, remote_port, recieve_first)
+
+if __name__ == '__main__':
+    main()
+
+# hex_dump("This is informtion that we will convert to HEX A")
