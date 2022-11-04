2db161d34f572a6e3914d1b67dec9c01b76790f6
agogeio <steven.aiello@agoge.io>
2022-11-04 06:14:02 -0400

    Working on the 'cd' command on the remote system

--- Chapter2-BasicNetworkingTools/bhp_ssh_client_v2.py
+++ Chapter2-BasicNetworkingTools/bhp_ssh_client_v2.py
@@ -6,6 +6,39 @@ import paramiko
 import shlex        # https://docs.python.org/3/library/shlex.html
 import subprocess
 
+def execute(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+
+    # print(shlex.split(cmd))
+    is_cd = cmd.split(' ')
+    cmd_to_exec = is_cd[0]
+
+    if cmd_to_exec == 'cd':
+        print('Trying to cd')
+        cd_path = is_cd[1] or '/home/'
+        os.chdir(cd_path)
+        
+    if (cmd_to_exec == 'cat') or (cmd_to_exec == 'type'):
+        # https://docs.python.org/3.10/library/subprocess.html?highlight=subprocess%20run#subprocess.run (below)
+        # had to use different subprocess.run settings for the cat command
+        cmd_result = subprocess.run(shlex.split(cmd), capture_output=True, text=True)
+
+        if cmd_result.returncode == 0:
+            return cmd_result.stdout + '\n'
+        else:
+            return cmd_result.stderr + '\n'
+
+    process_data = subprocess.run(shlex.split(cmd), shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+    
+    if process_data.returncode == 0:
+        return process_data.stdout
+        # return output
+    else:
+        return process_data.stderr
+
+
 def ssh_command(ip, port, user, passwd, command):
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -27,45 +60,20 @@ def ssh_command(ip, port, user, passwd, command):
                     break
 
                 print(shlex.split(cmd))
-                is_cd = cmd.split(' ')
-                cmd_to_exec = is_cd[0]
-
-                if cmd_to_exec == 'cd':
-                    cd_path = is_cd[1] or '/home/'
-                    os.chdir(cd_path)
-                    
-                if cmd_to_exec == 'cat' or 'type':
-                    # https://docs.python.org/3.10/library/subprocess.html?highlight=subprocess%20run#subprocess.run (below)
-                    # had to use different subprocess.run settings for the cat command
-                    cmd_result = subprocess.run(shlex.split(cmd), capture_output=True, text=True)
-
-                    if cmd_result.returncode == 0:
-                        ssh_session.send(str(cmd_result.stdout))
-                    else:
-                        ssh_session.send(str(cmd_result.stderr))
-
-
-                # process_data = subprocess.Popen(shlex.split(cmd), shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
-                # *** TESTING CONVERING EVERYTHING TO subprocess.run() *** 
-                process_data = subprocess.run(shlex.split(cmd), shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
-
-                ssh_session.send(process_data or 'okay')
-
-                #! Improve this for the cat command and changing dirs
-                #! take logic from the repcat code
-                # cmd_output = subprocess.check_output(shlex.split(cmd), shell=True)
-                # ssh_session.send(cmd_output or 'okay')
+                process_data = execute(cmd)
+                print(process_data)
+                ssh_session.send(process_data)
             except Exception as e:
-                ssh_session.send(str(e))
+                ssh_session.send(f'Error: {str(e)}')
         ssh_client.close()
     return
 
 if __name__ == '__main__':
     # user = getpass.getuser()
-    user = input('Enter username: ')
-    password = getpass.getpass()
+    user = input('Enter username: ') or 'user'
+    password = getpass.getpass() or 'secret'
 
-    ip = input('Enter server IP: ')
-    port = input('Enter server port: ')
+    ip = input('Enter server IP: ') or '192.168.1.100'
+    port = input('Enter server port: ') or 2222
     
 ssh_command(ip, port=port, user=user, passwd=password, command='ClientConnected')
