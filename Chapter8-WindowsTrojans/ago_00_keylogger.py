aa13f17c041c0c943e09e34cf7972a6cb49279f9
agogeio <steven.aiello@agoge.io>
2022-12-09 12:02:01 -0500

    Updated documentation for keylogger

--- Chapter8-WindowsTrojans/ago_00_keylogger.py
+++ Chapter8-WindowsTrojans/ago_00_keylogger.py
@@ -10,7 +10,13 @@ import pythoncom
 #? https://pypi.org/project/pywin32/
 #? Source from package author - https://github.com/mhammond/pywin32/issues/1739
 
+#? Also need C++ 14.0 or greater
+#* https://visualstudio.microsoft.com/visual-cpp-build-tools/
+
 import pyWinhook 
+#* https://www.swig.org/survey.html
+#* https://www.swig.org/download.html
+#* https://zwbetz.com/how-to-add-a-binary-to-your-path-on-macos-linux-windows/
 
 import sys
 import time
